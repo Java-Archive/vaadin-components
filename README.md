acf1ec8381ff093cfa7799c333e07364ffd4d72e
Sven Ruppert <sven.ruppert@gmail.com>
2018-10-01 09:15:06 +0200

    prepare repo - transition to vaadin-components

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # vaadin-components
 A collection of Vaadin components
+
+<center>
+<a href="https://vaadin.com">
+ <img src="https://vaadin.com/images/hero-reindeer.svg" width="200" height="200" /></a>
+</center>
+
+# Vaadin Components and more
+This is the wiki for the Vaadin Components.
+
+## Changelog / Version
+[![Maven Central](https://maven-badges.herokuapp.com/maven-central/org.rapidpm.vaadin/vaadin-components/badge.svg)](https://maven-badges.herokuapp.com/maven-central/org.rapidpm.vaadin/vaadin-components)
+
+
+### 00.01.00-RPM
+* Fluent API
+    * Component - the generic version
+    * TextField
+    * PasswordField
+    * Button
+    * Checkbox
+    * VerticalLayout
+    * HorizontalLayout
+
+* Component Login
+    * initial import / first version
+
+
+## Component Fluent API
+The missing Fluent API for Vaadin 10 Components.
+Documentation is : [Component-Fluent-API.md](Component-Fluent-API.md)
+
+## Component - Login
+The boring but usefull Login Component
+Documentation is : [Component-Login.md](Component-Login.md)
---
+++ m/login/src/test/java/junit/org/rapidpm/vaadin/demo/workspaces/ClickCounterWorkspacePageObject.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright © 2018 Sven Ruppert (sven.ruppert@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package junit.org.rapidpm.vaadin.demo.workspaces;
+
+import com.vaadin.flow.component.button.testbench.ButtonElement;
+import com.vaadin.flow.component.html.testbench.SpanElement;
+import org.openqa.selenium.WebDriver;
+import org.rapidpm.vaadin.addons.testbench.junit5.extensions.container.ContainerInfo;
+import org.rapidpm.vaadin.addons.testbench.junit5.pageobject.AbstractVaadinPageObject;
+
+import static java.lang.Integer.valueOf;
+
+public class ClickCounterWorkspacePageObject extends AbstractVaadinPageObject {
+
+
+  public ClickCounterWorkspacePageObject(WebDriver webdriver, ContainerInfo containerInfo) {
+    super(webdriver, containerInfo);
+  }
+
+  public ButtonElement btnClickMe() {
+    return btn().id(ClickCounterWorkspace.BTN_CLICK_ME_ID);
+  }
+
+  public SpanElement lbClickCount() {
+    return span().id(ClickCounterWorkspace.LB_CLICK_COUNT_ID);
+  }
+
+  public void click() {
+    btnClickMe().click();
+  }
+
+  public String clickCountAsString() {
+    return lbClickCount().getText();
+  }
+
+  // no exception handling
+  public int clickCount() {
+    return valueOf(clickCountAsString());
+  }
+
+}
+
+
